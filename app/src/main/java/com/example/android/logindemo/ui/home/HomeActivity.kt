0cd665d99a881ea1edf3d77e09a2133f5ef94ba2
vikrant arankalle <vikrant.arankalle@techcushy.com>
2020-07-26 19:36:29 +0530

    Added Home Screen Activity

---
+++ app/src/main/java/com/example/android/logindemo/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.logindemo.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.logindemo.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
